4681159fe0932ef640d9ecefcc9df15d320fef2d
MaximLKorenev <mlkorenev@yandex.ru>
2021-11-10 19:12:40 +0300

    new

---
+++ name_of_variable.py
@@ -0,0 +1,23 @@
+russia_first_time - goals_scored_by_Russia_in_the_first_half // голы забитые Россией в первом тайме
+
+junior - juniors_salary // зарплата джуна
+
+x - ship_speed_kmh // скорость корабля
+
+john - johns_gold_weight_kg // вес золота украденного Джоном
+
+month - month_number // номер месяца
+
+v - paint_volume // объем краски
+
+w - building_width_m // ширина здания в метрах
+
+l - building length_m // длина здания в метрах
+
+h - building_height_m // высота здания в метрах
+
+age - dogs_age_year // возраст песика в годах
+
+girl - girls_year_birth // год рождения девушки
+
+time - date_time_hour // время свидания в часах
