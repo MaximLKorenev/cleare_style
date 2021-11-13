c32df98bcbcc6dc3eba8739ffd0b353e31a71ced
MaximLKorenev <mlkorenev@yandex.ru>
2021-11-13 21:15:41 +0300

    new

---
+++ name_of_variable1.py
@@ -0,0 +1,38 @@
+6.1.
+v = int(input('Объем краски: ')) -> paint_volume = ...
+w = int(input('Ширина: '))       ->  weight_wall = ...
+l = int(input('Длина: '))        ->  lenght_wall = ...
+h = int(input('Высота: '))       ->  height_wall = ...
+s = 2 * (w + l) * h  -> square_wall = 2 * (weight_wall + lenght_wall) * height_wall
+
+sum1 -> sum_digit_left_middle
+sum2 -> sum_digit_right_middle
+
+x = [] -> list_random_number = []
+
+6.2.
+class Node
+class LinkedList
+class Queue
+class Stack
+
+6.3.
+class LinkedList:
+    def add_in_tail
+
+class DynArray
+    def make_array
+    def resize
+
+
+class NativeDictionary
+    def is_key
+    def put
+    def get
+
+6.4.
+ind      ->     index_key
+junior   ->     juniors_salary
+x        ->     ship_speed_kmh
+age      ->     dogs_age_year
+time     ->     date_time_hour
